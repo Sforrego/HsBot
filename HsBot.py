cf8a0beebf04c83b828ee18f9d7ab6d002fa03e6
Sebastian orrego <sforrego@uc.cl>
2020-01-20 01:06:34 -0300

    redy to test

--- HsBot.py
+++ HsBot.py
@@ -17,7 +17,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', s
 client = gspread.authorize(creds)
 bosses_sheet = client.open("07 Irons HiScore").get_worksheet(0)
 skills_sheet = client.open("07 Irons HiScore").get_worksheet(1)
-
+start_sheet = client.open("07 Irons HiScore").get_worksheet(2)
 
 
 #BOT
@@ -33,24 +33,24 @@ async def on_ready():
 @bot.command(name='add', help='Adds a player to the spreadsheets.')
 @commands.has_permissions(kick_members=True)
 async def add(ctx, name):
-    names = [x.lower() for x in bosses_sheet.col_values(1)[1:]]
-    update_player(bosses_sheet,skills_sheet,names,name,1)
+    names = [x.lower() for x in start_sheet.col_values(2)[1:]]
+    update_player(bosses_sheet,skills_sheet,start_sheet,names,name,1)
     response = f"{name} has been added to the memberslist."
     await ctx.send(response)
 
 @bot.command(name='update', help='Updates a players stats in the spreadsheets.')
 @commands.has_permissions(kick_members=True)
 async def update(ctx, name):
-    names = [x.lower() for x in bosses_sheet.col_values(1)[1:]]
-    update_player(bosses_sheet,skills_sheet,names,name,0)
+    names = [x.lower() for x in start_sheet.col_values(2)[1:]]
+    update_player(bosses_sheet,skills_sheet,start_sheet,names,name,0)
     response = f"{name} stats has been updated."
     await ctx.send(response)
 
 @bot.command(name='change', help='Changes a players rsn in the spreadsheets.')
 @commands.has_permissions(kick_members=True)
 async def change_rsn(ctx, old_name,new_name):
-    names = [x.lower() for x in bosses_sheet.col_values(1)[1:]]
-    update_rsn(bosses_sheet,skills_sheet,names,old_name,new_name)
+    names = [x.lower() for x in start_sheet.col_values(2)[1:]]
+    update_rsn(bosses_sheet,skills_sheet,start_sheet,names,old_name,new_name)
     response = f"{old_name} has been changed to {new_name} and his stats has been updated."
     await ctx.send(response)
 
@@ -59,32 +59,66 @@ async def change_rsn(ctx, old_name,new_name):
 async def full_update(ctx):
     msg = f"All players are being updated ..."
     await ctx.send(msg)
-    names = [x.lower() for x in bosses_sheet.col_values(1)[1:]]
+    names = [x.lower() for x in start_sheet.col_values(2)[1:]]
     update_all(bosses_sheet,skills_sheet,names)
     response = f"All players have been updated."
     await ctx.send(response)
 
 @bot.command(name='top', help='Shows the top 5 players and their kc for a specific stat.')
 async def top(ctx, stat):
-    names = [x.lower() for x in bosses_sheet.col_values(1)[1:]]
+    names = [x.lower() for x in start_sheet.col_values(2)[1:]]
     top_stats = top_stat(bosses_sheet, skills_sheet, names, stat, 5)
     if top_stats != 404:
         stat = get_stat(stat)
         response = f"{stat}\n\n"
-        for player in top_stats:
+        players = [x[0] for x in top_stats]
+        players = get_pretty_names(start_sheet,players)
+        for i,player in enumerate(top_stats,start=1):
             if len(player) == 2:
-                response += f"{player[1]} {player[0]}\n"
+                response += f"{i}. {player[1]} {players[i-1]}\n"
             elif len(player) == 3:
-                response += f"{player[2]} {player[1]} {player[0]}\n"
+                response += f"{i}. {player[2]} {player[1]} {players[i-1]}\n"
     else:
         response = f"Stat {stat} not found."
     await ctx.send(response)
 
-@bot.command(name='compare', help='Shows the top 5 players and their kc for a specific stat.')
+@bot.command(name='compare', help='Compares two players in a specific stat.')
 async def compare(ctx, stat, player1, player2):
-    names = [x.lower() for x in bosses_sheet.col_values(1)[1:]]
-    compare_players(bosses_sheet, skills_sheet, names, player1, player2, stat)
-    response = a
+    names = [x.lower() for x in start_sheet.col_values(2)[1:]]
+    p1,p2 = player1.lower(), player2.lower()
+    stat = get_stat(stat)
+    if not stat:
+        response = f"Stat not found."
+        await ctx.send(response)
+        return
+    print(f"{stat}")
+    if p1 not in names:
+        response = f"Player {p1} not found."
+        await ctx.send(response)
+        return
+    if p2 not in names:
+        response = f"Player {p2} not found."
+        await ctx.send(response)
+        return
+    comparison = compare_players(bosses_sheet, skills_sheet, names, p1, p2, stat)
+    winner = comparison[0]
+    player1,player2 = get_pretty_names(start_sheet,[p1,p2])
+    response = f"{stat} \n"
+    if len(comparison) == 5: #this is a skill
+        if winner:
+            response += f"1. {player1} {comparison[2]} {comparison[1]} \n"
+            response += f"2. {player2} {comparison[4]} {comparison[3]} "
+        else:
+            response += f"2. {player2} {comparison[4]} {comparison[3]} \n"
+            response += f"1. {player1} {comparison[2]} {comparison[1]} "
+    elif len(comparison) == 3:
+        if winner:
+            response += f"1. {player1} {comparison[1]}\n"
+            response += f"2. {player2} {comparison[2]}"
+        else:
+            response += f"2. {player2} {comparison[2]}\n"
+            response += f"1. {player1} {comparison[1]}"
+    await ctx.send(response)
 
 #
 #
--- funcs.py
+++ funcs.py
@@ -11,9 +11,16 @@ def get_stat(name):
     elif name in boss_shorts:
         return boss_shorts[name]
     else:
-        return 404
+        return 0
 
-
+def get_pretty_names(start_sheet,names):
+    pretty_names_list = []
+    for name in names:
+        ugly_names = start_sheet.col_values(2)[1:]
+        pretty_names = start_sheet.col_values(1)[1:]
+        index = ugly_names.index(name)
+        pretty_names_list.append(pretty_names[index])
+    return pretty_names_list   
 
 
 
@@ -24,8 +31,7 @@ def top_stat(bosses_sheet, skills_sheet, names, stat, n):
     elif stat not in SKILLS:
         index = STATSINDEX[stat]
         list = bosses_sheet.col_values(index)[1:]
-        list = [(int(list[i]),names[i]) for i in range(len(list))]
-
+        list = [(int(list[i]),names[i]) if list[i] != "" else (-1,names[i]) for i in range(len(list))]
     elif stat in SKILLS:
         index = SKILLS.index(stat)*2+1
         list = skills_sheet.get_all_values()[1:]
@@ -36,7 +42,7 @@ def top_stat(bosses_sheet, skills_sheet, names, stat, n):
     return list
 
 
-def update_player(bosses_sheet, skills_sheet, names, name, addplayer=0):
+def update_player(bosses_sheet, skills_sheet, start_sheet, names, name, addplayer=0):
     name = name.lower()
     if name not in names and not addplayer:
         print("Player not in memberslist.")
@@ -48,14 +54,26 @@ def update_player(bosses_sheet, skills_sheet, names, name, addplayer=0):
             if addplayer:
                 bosses_sheet.append_row([name])
                 names.append(name)
+
             index = names.index(name)+2
 
+
             skills_cell_list = skills_sheet.range(f'B{index}:AW{index}')
-            bosses_cell_list = bosses_sheet.range(f'B{index}:AX{index}')
+            bosses_cell_list = bosses_sheet.range(f'B{index}:AZ{index}')
             player_skills, player_clues , player_bosses = createDicts(parseStats(stats))
             player_bosses = [player_skills["Overall"]]+list(player_clues.values())+list(player_bosses.values())
+
+            if addplayer:
+                start_cell_list = start_sheet.range(f'B{index}:F{index}')
+                mylist = [name.replace(" ","_"),player_skills["Overall"],
+                player_skills["Overall"],player_skills["Overall_Xp"],player_skills["Overall_Xp"]]
+                for i,cell in enumerate(start_cell_list):
+                    cell.value = mylist[i]
+                start_sheet.update_cells(start_cell_list)
+
             player_skills = list(player_skills.values())
             print(f"updating {name} total {player_skills[0]}")
+
 
             for j,cell in enumerate(skills_cell_list):
                 cell.value = int(player_skills[j])
@@ -64,16 +82,19 @@ def update_player(bosses_sheet, skills_sheet, names, name, addplayer=0):
 
             skills_sheet.update_cells(skills_cell_list)
             bosses_sheet.update_cells(bosses_cell_list)
+
+
         else:
             print(f"{name} not found in hiscores.")
 
 
-def update_rsn(bosses_sheet, skills_sheet, names, old_name, new_name):
+def update_rsn(bosses_sheet, skills_sheet, start_sheet, names, old_name, new_name):
     old_name = old_name.lower()
     if old_name in names:
         index = names.index(old_name)+2
         bosses_sheet.update_acell(f'A{index}', new_name)
-        update_player(bosses_sheet, skills_sheet, names, new_name)
+        names[index] = new_name
+        update_player(bosses_sheet, skills_sheet, start_sheet, names, new_name)
 
 
 def update_all(bosses_sheet, skills_sheet, names, starting_cell=2):
@@ -131,18 +152,18 @@ def compare_players(bosses_sheet, skills_sheet, names, name1, name2, stat):
     name1,name2 = name1.lower(),name2.lower()
     index1 = names.index(name1)
     index2 = names.index(name2)
-    if stat in STATSINDEX.keys():
+    if stat in BOSSES+CLUES:
         index = STATSINDEX[stat]
         boss = bosses_sheet.col_values(index)[1:]
         score1,score2 = int(boss[index1]),int(boss[index2])
-        return   (1, score1,score2) if score1>score2 else (2,score1,score2)
+        return   (1, score1,score2) if score1>score2 else (0,score1,score2)
     elif stat in SKILLS:
         index = SKILLS.index(stat)*2+2
         skill = skills_sheet.col_values(index)[1:]
         skill_xp = skills_sheet.col_values(index+1)[1:]
         xp1,xp2 = int(skill_xp[index1]),int(skill_xp[index2])
         lvl1,lvl2 = int(skill[index1]),int(skill[index2])
-        return   (1, xp1,lvl1,xp2,lvl2) if xp1>xp2 else (2,xp1,lvl1,xp2,lvl2)
+        return   (1, xp1,lvl1,xp2,lvl2) if xp1>xp2 else (0,xp1,lvl1,xp2,lvl2)
     else:
         return 404
 
@@ -153,10 +174,14 @@ if __name__ == "__main__":
     client = gspread.authorize(creds)
     bosses_sheet = client.open("07 Irons HiScore").get_worksheet(0)
     skills_sheet = client.open("07 Irons HiScore").get_worksheet(1)
-    names = [x.lower() for x in bosses_sheet.col_values(1)[1:]]
-    #update_player(bosses_sheet,skills_sheet,names,"no ge canvey")
+    start_sheet = client.open("07 Irons HiScore").get_worksheet(2)
+    names = [x.lower() for x in start_sheet.col_values(2)[1:]]
+
+    #print(get_pretty_name(start_sheet,"no_ge_canvey"))
+
+    #print(compare_players(bosses_sheet, skills_sheet, names, "IronRok", "no ge canvey", "Attack"))
+    update_player(bosses_sheet,skills_sheet,start_sheet,names,"Iron Osc4r",1)
 
     #not_found = update_all(bosses_sheet,skills_sheet,names)
-    update_bosses_names(BOSSES,bosses_sheet)
     #print(compare_players(bosses_sheet,skills_sheet,names,"no ge canvey","ironrok","Magic"))
-    #print(top_stat(bosses_sheet,skills_sheet,names,"Attack",7))
+    #print(top_stat(bosses_sheet,skills_sheet,names,"Zulrah",7))
