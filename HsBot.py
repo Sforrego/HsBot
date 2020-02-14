81bf4d026faf9b258f337308ea704f08fa76808b
Sebastian orrego <sforrego@uc.cl>
2020-02-14 17:15:14 -0300

    a

--- HsBot.py
+++ HsBot.py
@@ -138,15 +138,18 @@ async def compare(ctx, stat, player1, player2):
             response += f"2. {player1} {comparison[1]}"
     await ctx.send(response)
 
-@bot1.command(name='memberslist', help='Shows every player and their join date.')
-@commands.has_permissions(kick_members=True)
-async def memberslit(ctx):
-    response = ""
-    members = ctx.guild.members
-    for member in members:
-        response += f"{member} {member.nick} Joined: {member.joined_at}.\n"
+# @bot1.command(name='memberslist', help='Shows every player and their join date.')
+# @commands.has_permissions(kick_members=True)
+# async def memberslit(ctx,num):
+#     response = ""
+#     members = ctx.guild.members
+#     for i,member in enumerate(members):
+#         response += f"{str(member)} {member.nick} Joined: {member.joined_at}.\n"
+#         if i == int(num):
+#             break
+#     await ctx.send(response)
 
-    await ctx.send(response)
+
 # @bot1.command(name='bossestop', help='Prints the top 5 players for every boss (Admin).')
 # @commands.has_permissions(kick_members=True)
 # async def full_bosses_print(ctx):
