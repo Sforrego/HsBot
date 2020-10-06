fcc44ad2faaaba79d8b0858af59db435cf940a15
Sebastián Orrego <sforrego@uc.cl>
2020-10-06 14:02:36 -0300

    new bingo board

--- image_editing.py
+++ image_editing.py
@@ -11,18 +11,18 @@ def read_image(path):
 
 def paint_tiles(tiles):
 
-    img_path = "Bingo_board.png"
+    img_path = "Bingo_board2.png"
     image = read_image(img_path)
     img_array = np.array(image)
 
     for tile in tiles:
         for i in range(tile//5*76, (tile//5)*76+75):
-            for j in range(tile%5*91, (tile%5)*91+90):
+            for j in range(tile%5*92, (tile%5)*91+90):
 
                 img_array[i][j] = np.array([0,200, 0, 255])
     return Image.fromarray(img_array)
 
 
 if __name__ == '__main__':
-    image = paint_tiles(img_array,[1,2])
+    image = paint_tiles([1,3,7,15,18,24])
     image.save('team1.png')
